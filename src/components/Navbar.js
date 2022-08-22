e9a4ec3e5f8f518ac2f70025d50e3932e8f076d0
Lucimara Souza <lucimarasouzah@gmail.com>
2022-08-22 11:25:50 -0300

    Adiciona estilização

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,10 +18,26 @@ const Navbar = () => {
         </li>
         <li>
           <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/register"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
             to="/about"
             className={({ isActive }) => (isActive ? styles.active : "")}
           >
-            About
+            Sobre
           </NavLink>
         </li>
       </ul>
